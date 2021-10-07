3ea201aa27b7f718981d1637c6e4f43f0e4a46b6
kalpak44 <avgustares@gmail.com>
2021-10-07 20:24:21 +0300

    first steps to design

--- src/main/java/com/givehost/web/controller/SignUpController.java
+++ src/main/java/com/givehost/web/controller/SignUpController.java
@@ -1,27 +1,13 @@
 package com.givehost.web.controller;
 
-import com.givehost.web.model.User;
-import com.givehost.web.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SignUpController {
-    private UserRepository userRepository;
-
-    public SignUpController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @GetMapping("/sign-up")
     public String showUserList(Model model) {
-        System.out.println("sign up");
-        User n = new User();
-        n.setName("user");
-        n.setEmail("user@mail.com");
-        userRepository.save(n);
-
-        return "/pages/sign-up";
+        return "/pages/register-page";
     }
 }
