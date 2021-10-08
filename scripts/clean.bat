16ec153b6d55b06a54377f2c8b4d208f75fb2ff9
kalpak44 <avgustares@gmail.com>
2021-10-08 18:45:00 +0300

    cleanup

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -1,3 +1,4 @@
 FOR /f "tokens=*" %%i IN ('docker ps -qa') DO docker stop %%i
 FOR /f "tokens=*" %%i IN ('docker ps -qa') DO docker rm %%i
 FOR /f "tokens=*" %%i IN ('docker images -qa') DO docker rmi %%i
+docker volume prune -f
